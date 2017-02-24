25f3e91b189a3f88ea35de7a31ccc265d2d5b256
Mike Hudgins <mchudgins@dstsystems.com>
2017-02-23 18:03:18 -0600

    added some prometheus metrics

--- pkg/cmd/backend/backend.go
+++ pkg/cmd/backend/backend.go
@@ -8,9 +8,12 @@ import (
 	"strconv"
 	"syscall"
 	"text/template"
+	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/afex/hystrix-go/hystrix/metric_collector"
 	"github.com/mchudgins/certMgr/pkg/healthz"
+	"github.com/mchudgins/go-service-helper/hystrix"
 	"github.com/mchudgins/go-service-helper/loggingWriter"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -30,7 +33,9 @@ var (
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Welcome to OpenShift</title>
-
+  <p>This is {{.Hostname}}</p>
+  <p>Page: {{.URL}}</p>
+  <p>Handler: {{.Handler}}</p>
 </body>
 </html>`
 
@@ -48,11 +53,19 @@ var (
 		},
 		[]string{"url", "status"},
 	)
+	httpRequestDuration = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "http_response_duration",
+			Help: "Duration of HTTP responses.",
+		},
+		[]string{"url", "status"},
+	)
 )
 
 func init() {
 	prometheus.MustRegister(httpRequestsReceived)
 	prometheus.MustRegister(httpRequestsProcessed)
+	prometheus.MustRegister(httpRequestDuration)
 
 	indexTemplate = template.Must(template.New("/").Parse(html))
 }
@@ -92,11 +105,17 @@ func (l *promWriter) StatusCode() int {
 
 func httpCounter(fn http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
 		u := r.URL.Path
 		httpRequestsReceived.With(prometheus.Labels{"url": u}).Inc()
 		pw := NewPromWriter(w)
 		defer func() {
-			httpRequestsProcessed.With(prometheus.Labels{"url": u, "status": strconv.Itoa(pw.statusCode)}).Inc()
+			status := strconv.Itoa(pw.statusCode)
+			httpRequestsProcessed.With(prometheus.Labels{"url": u, "status": status}).Inc()
+			end := time.Now()
+			duration := end.Sub(start)
+			httpRequestDuration.With(prometheus.Labels{"url": u, "status": status}).Observe(float64(duration.Nanoseconds()))
 		}()
 
 		fn.ServeHTTP(pw, r)
@@ -135,14 +154,41 @@ func Run() error {
 
 		mux.Handle("/healthz", healthzHandler)
 		mux.Handle("/metrics", prometheus.Handler())
+
+		apiMux := http.NewServeMux()
+		apiMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			httpRequestsReceived.With(prometheus.Labels{"url": r.URL.Path}).Inc()
+
+			type data struct {
+				Hostname string
+				URL      string
+				Handler  string
+			}
+
+			err = indexTemplate.Execute(w, data{Hostname: hostname, URL: r.URL.Path, Handler: "/api/v1"})
+			if err != nil {
+				log.WithError(err).Error("Unable to execute template")
+			}
+
+			httpRequestsProcessed.With(prometheus.Labels{"url": r.URL.Path, "status": "200"}).Inc()
+		})
+		circuitBreaker, err := hystrix.NewHystrixHelper("grpc-backend")
+		if err != nil {
+			log.Fatalf("Error creating circuitBreaker: %s", err)
+		}
+		metricCollector.Registry.Register(circuitBreaker.NewPrometheusCollector)
+		mux.Handle("/api/v1/", circuitBreaker.Handler(apiMux))
+
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			httpRequestsReceived.With(prometheus.Labels{"url": "/"}).Inc()
 
 			type data struct {
 				Hostname string
+				URL      string
+				Handler  string
 			}
 
-			err = indexTemplate.Execute(w, data{Hostname: hostname})
+			err = indexTemplate.Execute(w, data{Hostname: hostname, URL: r.URL.Path, Handler: "/"})
 			if err != nil {
 				log.WithError(err).Error("Unable to execute template")
 			}
