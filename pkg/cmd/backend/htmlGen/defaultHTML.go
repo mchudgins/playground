705436242f7e39a3c2a70b433e0cab8804d0ae6e
Mike Hudgins <mchudgins@dstsystems.com>
2017-03-14 16:42:53 -0500

    tweaked settings for legibility (again)

--- pkg/cmd/backend/htmlGen/defaultHTML.go
+++ pkg/cmd/backend/htmlGen/defaultHTML.go
@@ -7,6 +7,7 @@ const DefaultHTML = `<!doctype html>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>DST API Explorer</title>
   <style>
+  	h1 { background-color: #f0f0ff; padding-top: 1em; padding-bottom: 1em; }
   	div.row { margin-bottom: 0.75em; padding-top: .25em; padding-bottom: .25em; }
   	div.row:nth-child(odd) { background-color: whitesmoke; }
   	div.row:nth-child(even) { background-color: white; }
@@ -16,11 +17,13 @@ const DefaultHTML = `<!doctype html>
   	.active_status { color: blue; }
   	.deprecated_status { color: gray; }
   	.deprecated_desc_status::after { content: " (deprecated)"; }
-  	.name_col { display: inline-block; width: 7em; }
-  	.version_col { display: inline-block; width: 5em; }
-  	.desc_col { display: inline-block; width: 60em; }
+  	.apiSet_name_col { display: inline-block; }
+  	.apiSet_desc_col { display: inline-block; }
+  	.name_col { display: inline-block; width: 4em; }
+  	.version_col { display: inline-block; width: 4em; }
+  	.desc_col { display: inline-block; width: 30em; }
   	.apiEndpoint { display: block; margin-left: 4em; }
-  	.apiSet { font-size: 110%; font-weight: bold; }
+  	.apiSet { font-size: 110%; font-weight: bold; margin-bottom: .25em; }
   </style>
 </head>
 <body>
@@ -29,8 +32,7 @@ const DefaultHTML = `<!doctype html>
 {{range .APIList}}
 <div class="row">
 	<div class="apiSet">
-		<div class="name_col">{{.Name}}</div>
-		<div class="desc_col">{{.Desc}}</div>
+		<div class="apiSet_name_col">{{.Name}}</div>&nbsp;:&nbsp;<div class="apiSet_desc_col">{{.Desc}}</div>
 	</div>
 	{{range .Endpoints}}
 	<div class="apiEndpoint {{.Status}}_status">
