c9f3fb29f05e94fbf6657b0e22f3f03f91881a40
Mike Hudgins <mchudgins@gmail.com>
2017-06-11 15:02:47 -0500

    serve up index.html when a directory is requested

---
+++ tmp/server.go
@@ -0,0 +1,67 @@
+//go:generate staticfiles -o hugo.go ../../../../../src/hugo/public
+
+package tmp
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+)
+
+// ServeHTTP serves a request, attempting to reply with an embedded file.
+// It improves on the provided ServeHTTP by checking if an 'index.html'
+// file exists.
+func ServeHTTPWithIndexes(rw http.ResponseWriter, req *http.Request) {
+	// configure tracing
+	span, ctx := opentracing.StartSpanFromContext(req.Context(), "ServeHTTPWithIndexes")
+	req = req.WithContext(ctx)
+	defer span.Finish()
+
+	// now, serve the request
+	f, ok := staticFiles[strings.TrimPrefix(req.URL.Path, "/")]
+	if !ok {
+		f, ok = staticFiles[strings.TrimSuffix(strings.TrimPrefix(req.URL.Path, "/"), "/")+"/index.html"]
+		if !ok {
+			NotFound(rw, req)
+			return
+		}
+	}
+	header := rw.Header()
+	if f.hash != "" {
+		if hash := req.Header.Get("If-None-Match"); hash == f.hash {
+			rw.WriteHeader(http.StatusNotModified)
+			return
+		}
+		header.Set("ETag", f.hash)
+	}
+	if !f.mtime.IsZero() {
+		if t, err := time.Parse(http.TimeFormat, req.Header.Get("If-Modified-Since")); err == nil && f.mtime.Before(t.Add(1*time.Second)) {
+			rw.WriteHeader(http.StatusNotModified)
+			return
+		}
+		header.Set("Last-Modified", f.mtime.UTC().Format(http.TimeFormat))
+	}
+	header.Set("Content-Type", f.mime)
+
+	// Check if the asset is compressed in the binary
+	if f.size == 0 {
+		header.Set("Content-Length", strconv.Itoa(len(f.data)))
+		io.WriteString(rw, f.data)
+	} else {
+		if header.Get("Content-Encoding") == "" && strings.Contains(req.Header.Get("Accept-Encoding"), "gzip") {
+			header.Set("Content-Encoding", "gzip")
+			header.Set("Content-Length", strconv.Itoa(len(f.data)))
+			io.WriteString(rw, f.data)
+		} else {
+			header.Set("Content-Length", strconv.Itoa(f.size))
+			reader, _ := gzip.NewReader(strings.NewReader(f.data))
+			io.Copy(rw, reader)
+			reader.Close()
+		}
+	}
+}
