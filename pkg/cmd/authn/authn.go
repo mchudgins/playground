195300712202cd32d12df9dc5f0d81a4e3e50751
Mike Hudgins <mchudgins@gmail.com>
2017-06-25 16:23:13 -0500

    replace actuator with gorilla mux router; add jwt creation

--- pkg/cmd/authn/authn.go
+++ pkg/cmd/authn/authn.go
@@ -7,15 +7,17 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"encoding/json"
 	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/afex/hystrix-go/hystrix/metric_collector"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
-	"github.com/mchudgins/go-service-helper/actuator"
 	gsh "github.com/mchudgins/go-service-helper/handlers"
 	"github.com/mchudgins/go-service-helper/hystrix"
 	"github.com/mchudgins/playground/pkg/healthz"
@@ -44,7 +46,7 @@ func Run(port, host string) error {
 
 	// http server
 	go func() {
-		mux := actuator.NewActuatorMux("")
+		mux := mux.NewRouter() //actuator.NewActuatorMux("")
 
 		hc, err := healthz.NewConfig()
 		healthzHandler, err := healthz.Handler(hc)
@@ -55,6 +57,8 @@ func Run(port, host string) error {
 		mux.Handle("/debug/vars", expvar.Handler())
 		mux.Handle("/healthz", healthzHandler)
 		mux.Handle("/metrics", prometheus.Handler())
+		mux.HandleFunc("/login", loginGetHandler).Methods("GET")
+		mux.HandleFunc("/login", loginPostHandler).Methods("POST")
 
 		apiMux := http.NewServeMux()
 		apiMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -67,14 +71,30 @@ func Run(port, host string) error {
 
 			const authURL string = "/api/v1/authenticate/"
 
+			logger, _ := gsh.FromContext(r.Context())
+
 			if strings.HasPrefix(r.URL.Path, authURL) {
+				uid := r.URL.Path[len(authURL):]
+				now := time.Now()
+				token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+					Subject:   uid,
+					ExpiresAt: now.Add(time.Duration(5) * time.Minute).Unix(),
+					Audience:  "*.dstcorp.net",
+					Issuer:    "authn.dstcorp.net",
+					IssuedAt:  now.Unix(),
+					NotBefore: now.Add(0 - time.Duration(30)*time.Second).Unix(),
+				}).SignedString([]byte("hello, world"))
+				if err != nil {
+					logger.WithError(err).Fatal("signing token")
+				}
+
 				m := &AuthResponse{
-					JWT:    "asldgk45cvmop8avppM",
-					UserID: r.URL.Path[len(authURL):],
+					JWT:    token,
+					UserID: uid,
 				}
 				buf, err := json.Marshal(m)
 				if err != nil {
-					log.WithError(err).WithField("authResponse", m.UserID).
+					logger.WithError(err).WithField("authResponse", m.UserID).
 						Error("while serializing auth response")
 					w.WriteHeader(http.StatusInternalServerError)
 				} else {
@@ -97,8 +117,12 @@ func Run(port, host string) error {
 
 		canonical := handlers.CanonicalHost(host, http.StatusPermanentRedirect)
 		var tracer func(http.Handler) http.Handler
-		tracer = gsh.TracerFromInternalHTTPRequest(gsh.NewTracer("authn"), "authn")
-		chain := alice.New(tracer, gsh.HTTPMetricsCollector, gsh.HTTPLogrusLogger, canonical).Then(mux)
+		tracer = gsh.TracerFromHTTPRequest(gsh.NewTracer("authn"), "authn")
+		chain := alice.New(tracer,
+			gsh.HTTPMetricsCollector,
+			gsh.HTTPLogrusLogger,
+			handlers.CompressHandler,
+			canonical).Then(mux)
 
 		log.WithField("port", port).Info("HTTP service listening.")
 		errc <- http.ListenAndServe(port, chain)
