cc985dce102aa6127c6015442ec23a993bc1ac3b
Mike Hudgins <mchudgins@gmail.com>
2017-09-02 15:44:06 -0500

    add describe-cert

--- cmd/describe-cert.go
+++ cmd/describe-cert.go
@@ -16,13 +16,31 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"os"
+
+	"io/ioutil"
+
+	"encoding/pem"
+
+	"crypto/x509"
 
 	"github.com/spf13/cobra"
 )
 
+type CertificateDescription struct {
+	Subject          string
+	AlternativeNames []string
+	Issuer           string
+}
+
+func (cd *CertificateDescription) ToString() string {
+	return fmt.Sprintf("%s, %s, %s", cd.Subject, cd.AlternativeNames, cd.Issuer)
+}
+
 // describe-certCmd represents the describe-cert command
 var describeCertCmd = &cobra.Command{
-	Use:   "describe-cert",
+	Use:   "describe-cert [certificate filenames]",
 	Short: "display important details of an x509 certificate",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -31,9 +49,55 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("describe-cert called")
+		var fileList []*os.File
+
+		filecount := len(args)
+		if filecount == 0 {
+			fileList = make([]*os.File, 1, 1)
+			fileList[0] = os.Stdin
+		} else {
+			fileList = make([]*os.File, filecount, filecount)
+			for i, filename := range args {
+				var err error
+				fileList[i], err = os.OpenFile(filename, os.O_RDONLY, 0)
+				defer fileList[i].Close()
+				if err != nil {
+					fmt.Fprintf(cmd.OutOrStderr(), "%s while attempting to open %s.\n", err, filename)
+				}
+			}
+		}
+
+		for i, file := range fileList {
+			cd, err := describe(file)
+			if err != nil {
+				fmt.Fprint(cmd.OutOrStderr(), "while accessing %s -- %s", args[i], err)
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), "%s, %s\n", args[i], cd.ToString())
+		}
+
 	},
+}
+
+func describe(file io.Reader) (*CertificateDescription, error) {
+	buf, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	p, _ := pem.Decode(buf)
+	if p == nil {
+		panic("failed to decode certificate")
+	}
+	cert, err := x509.ParseCertificate(p.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return &CertificateDescription{
+		Subject:          cert.Subject.CommonName,
+		AlternativeNames: cert.DNSNames,
+		Issuer:           cert.Issuer.CommonName,
+	}, nil
 }
 
 func init() {
