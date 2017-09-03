0bf7de5a2248d404bb32425914db1b182745b048
Mike Hudgins <mchudgins@gmail.com>
2017-09-02 23:34:47 -0500

    clean up echoService test

--- echo/echoService_test.go
+++ echo/echoService_test.go
@@ -1,11 +1,11 @@
 package echo
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
-	"testing"
-
 	"strings"
+	"testing"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -63,34 +63,56 @@ func TestGetURLs(t *testing.T) {
 	}
 }
 
+const expected = "this is what we expect to be echo-ed"
+
+var methodTests = []struct {
+	url          string
+	method       string
+	responseBody string
+	statusCode   int
+}{
+	{"/", "POST", expected, http.StatusOK},
+	{"/", "GET", "", http.StatusOK},
+	{"/", "PUT", "", http.StatusMethodNotAllowed},
+	{"/", "HEAD", "", http.StatusMethodNotAllowed},
+	{"/", "DELETE", "", http.StatusMethodNotAllowed},
+	{"/", "OPTIONS", "", http.StatusMethodNotAllowed},
+}
+
 func TestEchoPost(t *testing.T) {
 
-	const expected = "this is what we expect to be echo-ed"
+	for _, tt := range methodTests {
+		// create a request to pass to the handler
+		var reader io.Reader
+		if len(tt.responseBody) != 0 {
+			reader = strings.NewReader(tt.responseBody)
+		} else {
+			reader = nil
+		}
+		req, err := http.NewRequest(tt.method, tt.url, reader)
+		if err != nil {
+			t.Fatalf("unable to create request (http.NewRequest) -- %s", err)
+		}
 
-	// create a request to pass to the handler
-	req, err := http.NewRequest("POST", "/", strings.NewReader(expected))
-	if err != nil {
-		t.Fatalf("unable to create request (http.NewRequest) -- %s", err)
-	}
+		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+		rr := httptest.NewRecorder()
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
+		// the handler under test
+		handler := NewHTTPServer(getLogger())
 
-	// the handler under test
-	handler := NewHTTPServer(getLogger())
+		// perform test
+		handler.ServeHTTP(rr, req)
 
-	// perform test
-	handler.ServeHTTP(rr, req)
+		// Check the status code is what we expect.
+		if status := rr.Code; status != tt.statusCode {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, tt.statusCode)
+		}
 
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	// Check the response body is what we expect.
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+		// Check the response body is what we expect.
+		if len(tt.responseBody) != 0 && rr.Body.String() != tt.responseBody {
+			t.Errorf("%s handler returned unexpected body: got '%v' want '%v'",
+				tt.method, rr.Body.String(), tt.responseBody)
+		}
 	}
 }
